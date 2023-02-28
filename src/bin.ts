cbb1e5c2f1c1dd8ff4d95f806243b7bcb1ae9218
4Sight <116753476+4S1ght@users.noreply.github.com>
2023-02-28 10:38:20 +0100

    Added basic bin script implementation.

---
+++ src/bin.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+// ============================================
+// To test/debug nodebff live in the terminal do:
+// > npm link
+// > npm i -g .
+// FROM the project directory
+// ============================================
+
+console.log("--> Node BFF")
